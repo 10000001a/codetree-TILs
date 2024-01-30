db5d819138cd5fc633e1b0b70d406f438bdc4945
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-01-30 21:22:50 +0900

    Title: 입력받아 출력 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240130/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter("-");
+
+        String f = sc.next();
+        String b = sc.next();
+
+        System.out.println(f+b);
+        // 여기에 코드를 작성해주세요.
+    }
+}
