9317d15b81cd78e7a18a2eec0bc3a64fd38f71fd
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-01-31 02:55:52 +0900

    Title: 전화번호 바꾸기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240131/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nums = sc.next().split("-");
+
+        System.out.println("010-" + nums[2] + "-" + nums[1]);
+        // 여기에 코드를 작성해주세요.
+    }
+}
