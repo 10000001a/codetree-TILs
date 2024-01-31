4974e39555ba8625617c238e9c3bc5e1beb90177
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-01-31 11:11:46 +0900

    Title: 특정 숫자 도달하기, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240131/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useDelimiter(" ");
+
+        int sum = 0;
+        int count = 0;
+        boolean check = false;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n >= 250) {
+                check = true;
+            }
+
+            if (!check) {
+                sum += n;
+                count ++;
+            }
+        }
+        System.out.print(sum);
+        System.out.print(' ');
+        System.out.println(((double) sum / count));
+
+        // System.out.println(sum + ' ' + ((double) sum / count));
+        
+
+        
+        // 여기에 코드를 작성해주세요.
+    }
+}
