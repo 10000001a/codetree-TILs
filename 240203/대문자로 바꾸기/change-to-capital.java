d78b4f7c16c2538de4d77ec6370bfd942f09e23a
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-02-03 02:19:02 +0900

    Title: 대문자로 바꾸기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/대문자로 바꾸기/change-to-capital.java
+++ 240203/대문자로 바꾸기/change-to-capital.java
@@ -5,13 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         for (int i=0; i<5; i++) {
-            for (int j=0; j<3; j++) {
+
                 String a = sc.next();
                 String b = sc.next();
                 String c = sc.next();
 
                 System.out.println(a.toUpperCase() + " " +b.toUpperCase() + " " + c.toUpperCase() );
-            }
+
         }
         // 여기에 코드를 작성해주세요.
     }
