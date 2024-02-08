d2615d224297b68d64696d3665a00150be78b287
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-02-08 16:33:00 +0900

    Title: 원소의 합이 0, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,37 @@
+import sys
+
+IN = sys.stdin.readline
+
+n = int(IN());
+
+A = list(map(int, IN()[:-1].split(' ')))
+B = list(map(int, IN()[:-1].split(' ')))
+C = list(map(int, IN()[:-1].split(' ')))
+D = list(map(int, IN()[:-1].split(' ')))
+
+dict_ab = {}
+dict_cd = {}
+
+for a in A:
+    for b in B:
+        s = a + b
+        if s in dict_ab:
+            dict_ab[s] += 1
+        else:
+            dict_ab[s] = 1
+
+for a in C:
+    for b in D:
+        s = a + b
+        if s in dict_cd:
+            dict_cd[s] += 1
+        else:
+            dict_cd[s] = 1
+
+ans = 0
+
+for k in dict_ab:
+    if -k in dict_cd:
+        ans += dict_ab[k] * dict_cd[-k]
+
+print(ans)
