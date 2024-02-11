16ff939fec3ca13f25b45f511db2d0592c96b0f3
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-02-12 02:17:32 +0900

    Title: 자리 바꾸기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240212/자리 바꾸기 2/changing-seats-2.py
+++ 240212/자리 바꾸기 2/changing-seats-2.py
@@ -4,16 +4,25 @@ IN = sys.stdin.readline
 
 N, K = map(int, IN().split())
 
-x = [set([i]) for i in range(K + 1)]
+x = [set([i]) for i in range(N + 1)]
 
 seat = {
     i: i for i in range(K + 1)
 }
 
-print(seat)
-
 change = [
     tuple(map(int, IN().split())) for _ in range(K) 
 ]
 
-print(change)
+for _ in range(3):
+    for a, b in change:
+        tmp = seat[a]
+        seat[a] = seat[b]
+        seat[b] = tmp
+
+        x[seat[a]].add(a)
+        x[seat[b]].add(b)
+
+
+for i in range(1, N + 1):
+    print(len(x[i]))
