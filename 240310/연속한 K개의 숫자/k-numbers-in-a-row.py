1210ccedd0938e8e53ba278205ec66542daa4f0c
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-03-10 11:27:45 +0900

    Title: 연속한 K개의 숫자, Time: 148ms, Memory: 28MB, Status: Passed - CodeTree

--- 240310/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240310/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -12,21 +12,15 @@ for _ in range(b):
 
 ans = [1 if 1 in dropped else 0]
 
-# print(ans)
-
 for i in range(2, k + 1):
-    # print('i: ',i)
+
     if i in dropped:
         ans.append(ans[i - 2] + 1)
     else:
         ans.append(ans[i - 2])
 
-# print(ans)
-
 for i in range(k + 1, n + 1):
     da = 0
-
-    # print(i - k, i)
 
     if i - k in dropped:
         da -= 1
@@ -35,9 +29,5 @@ for i in range(k + 1, n + 1):
         da += 1
 
     ans.append(ans[i - 2] + da)
-
-#     3 4   6 7 8
-# 1 2     5       9 10
-# print(ans)
     
 print(min(ans[k - 1:]))
