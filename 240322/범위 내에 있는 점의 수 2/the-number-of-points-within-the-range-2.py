75dcbf5467afa221ee3b8c597a854b28fc99215b
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-03-22 13:03:30 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 353ms, Memory: 51MB, Status: Wrong Answer - CodeTree

--- 240322/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 240322/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -16,9 +16,14 @@ for i in range(1, dots[-1]+ 1):
     else:
         sum_.append(sum_[-1])
 
+
 for _ in range(Q):
     x, y = map(int, IN().split())
     x = x if x > 0 else 0
+    if x >= len(sum_):
+        print(0)
+        continue
+
     y = min(y, len(sum_) - 1)
     
     print(sum_[y] - sum_[x - 1])
