2a324028d6696ca79541f88b52bf661a135f97ce
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-08-18 18:18:11 +0900

    Title: 피보나치 수, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240818/피보나치 수/fibonacci-number.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    static int N;
+    static final int MAX_N = 45;
+    static int[] memo = new int[MAX_N + 1];
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        for (int i = 0; i < MAX_N + 1; i++) {
+            memo[i] = -1;
+        }
+
+        System.out.println(fib(N));
+    }
+
+    private static int fib(int n) {
+        if (memo[n] >= 0) {
+            return memo[n];
+        }
+        if (n == 0) {
+            memo[n] = 0;
+            return 0;
+        }
+
+        if (n == 1) {
+            memo[n] = 1;
+            return 1;
+        }
+
+        if (n == 2) {
+            memo[n] = 1;
+            return 1;
+        }
+
+        if (n == 3) {
+            memo[n] = 2;
+            return 2;
+        }
+
+        memo[n] = fib(n - 1) + fib(n - 2);
+
+        return memo[n];
+    }
+}
