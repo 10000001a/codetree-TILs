0e7d8783c7b22d3a1734e6b48d53c4f33f2f3cef
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-09-21 20:03:40 +0900

    Title: 2차원 최대 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240921/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    static int N, M;
+    static int[][] table;
+    static int[][] dp;
+    static int answer = 0;
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        table = new int[N + 1][M + 1];
+        dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                table[i][j] = sc.nextInt();
+            }
+        }
+
+        init();
+
+        for (int i = 4; i < (N + M + 1); i++){
+            if (i - 2 > N) {
+                for (int j = (i - (N)); j <= M; j++) {
+                    // System.out.println("-i: " + (i - j) + ", j: " + j);
+
+                    setDP(i - j, j);
+                }
+            } else {
+                for (int j = 2; j <= Math.min(i - 2, M); j++) {
+                    // System.out.println("i: " + (i - j) + ", j: " + j);
+                    
+                    setDP(i - j, j);
+                    
+                }
+            }
+        }
+
+        // printDP();
+        printAnswer();
+    }
+
+    private static void init() {
+        dp[1][1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i][1] = -1;
+        }
+
+        for (int i = 2; i <= M; i++) {
+            dp[1][i] = -1;
+        }
+    }
+
+    private static void setDP(int x, int y) {
+        int c = table[x][y];
+        for (int i = 1; i < x; i++) {
+            for (int j = 1; j < y; j++) {
+                if (table[i][j] < c) {
+                    dp[x][y] = Math.max(dp[x][y], dp[i][j] + 1);
+                }
+            }
+        }
+    }
+
+    private static void printTable() {
+        for (int i = 1; i <= N; i++) {
+            System.out.println(
+                Arrays.stream(table[i])
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "))
+            );
+        }
+    }
+
+    private static void printDP() {
+        for (int i = 1; i <= N; i++) {
+            System.out.println(
+                Arrays.stream(dp[i])
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "))
+            );
+        }
+    }
+
+    private static void printAnswer() {
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (dp[i][j] > answer) {
+                    answer = dp[i][j];
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
