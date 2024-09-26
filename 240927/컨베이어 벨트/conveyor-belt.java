e4aa88ad8ee069a220dfb1dfe68c43ea84fb1565
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-09-27 00:45:55 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240927/컨베이어 벨트/conveyor-belt.java
+++ 240927/컨베이어 벨트/conveyor-belt.java
@@ -14,25 +14,28 @@ public class Main {
         belt = new int[3][N + 1];
 
         
-        for (int j = 1; j <= N; j++) {
-            belt[1][j] = sc.nextInt();
+        for (int a = 0; a < T; a++) {
+            for (int j = 1; j <= N; j++) {
+                belt[1][j] = sc.nextInt();
+            }
+
+            for (int j = 1; j <= N; j++) {
+                belt[2][j] = sc.nextInt();
+            }
+
+            int tmp = belt[1][N];
+
+            for (int j = N; j >= 2; j--) {
+                belt[1][j] = belt[1][j - 1];
+            }
+            belt[1][1] = belt[2][N];
+
+            for (int j = N; j >= 2; j--) {
+                belt[2][j] = belt[2][j - 1];
+            }
+            belt[2][1] = tmp;
         }
 
-        for (int j = 1; j <= N; j++) {
-            belt[2][j] = sc.nextInt();
-        }
-
-        int tmp = belt[1][N];
-
-        for (int j = N; j >= 2; j--) {
-            belt[1][j] = belt[1][j - 1];
-        }
-        belt[1][1] = belt[2][N];
-
-        for (int j = N; j >= 2; j--) {
-            belt[2][j] = belt[2][j - 1];
-        }
-        belt[2][1] = tmp;
 
         System.out.println(
             Arrays.stream(Arrays.copyOfRange(belt[1], 1, N + 1))
