025915a5d574f6a094c5401e3f5ed6d10c5fcaab
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-01-06 00:54:02 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 867ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250106/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class X implements Comparable<X> {
+    final int l;
+    final int r;
+    final Long answer;
+
+    X(String s) {
+        int l = 0;
+        int r = 0;
+        long a = 0L;
+
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                l++;
+            } else {
+                r++;
+                a += l;
+            }
+        }
+
+        this.l = l;
+        this.r = r;
+        this.answer = a;
+    }
+
+    @Override
+    public int compareTo(X o) {
+        return o.l * this.r - this.l * o.r;
+    }
+}
+
+public class Main {
+    static int N;
+    static Scanner sc = new Scanner(System.in);
+    static X[] inputs;
+    static Long restR = 0L;
+
+    public static void main(String[] args) {
+        N = sc.nextInt();
+        sc.nextLine();
+
+        inputs = new X[N];
+
+        for (int i = 0; i < N; i++) {
+            inputs[i] = new X(sc.nextLine());
+            restR += inputs[i].r;
+        }
+
+        Arrays.sort(inputs);
+
+
+        System.out.println(calc());
+    }
+
+    private static Long calc() {
+        Long answer = 0L;
+
+        for (int i = 0; i < N; i++) {
+            final X target = inputs[i];
+
+            answer += target.answer;
+            restR -= target.r;
+            if (restR > 0)
+                answer += target.l * restR;
+        }
+
+        return answer;
+    }
+}
