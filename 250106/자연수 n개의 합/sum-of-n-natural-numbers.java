f14422fab3b87e0ad36b5687e6acdf3130f8b052
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-01-06 23:01:57 +0900

    Title: 자연수 n개의 합, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 250106/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 250106/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -9,20 +9,28 @@ public class Main {
 
         S = sc.nextLong();
 
-        int start = (int) Math.round(Math.sqrt((double) S * 2));
-        long sum = (long) ((float)(1 + start) / 2 * start);
+        int start = (int) Math.floor(Math.sqrt((double) S * 2));
 
-//        System.out.println(start);
-//        System.out.println(sum);
+        long sum = start % 2 == 1 ? (long) (start + 1) / 2 * start : (long) (start + 1) * (start / 2);
+
+
+//        System.out.println("start: " + start);
+//        System.out.println("sum: " + sum);
 
         while (sum > S) {
+//            System.out.println("1, " + sum + ", " + S);
             sum -= start;
             start--;
+//            System.out.println("start: " + start);
+//            System.out.println("sum: " + sum);
         }
 
-        while (S - sum > start) {
+        while (S > sum + start + 1) {
+//            System.out.println("2, " + (S - sum) + ", " + start);
             start++;
             sum += start;
+//            System.out.println("start: " + start);
+//            System.out.println("sum: " + sum);
         }
 
         System.out.println(start);
