4777b02ec17e67bc3a88f6cb5f81dba6773f1796
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-01-26 15:16:10 +0900

    Title: 사다리 타기, Time: 309ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250126/사다리 타기/ladder-game.java
+++ 250126/사다리 타기/ladder-game.java
@@ -101,7 +101,7 @@ public class Main {
 
     private static int c() {
         int answer = 0;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < N; i++) {
             for (int j = i + 1; j <= N; j++) {
                 int finalI = i;
                 int finalJ = j;
