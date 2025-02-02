56644fd2bc93a5e41941f50e9f03bf9527f111de
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-02-02 14:32:31 +0900

    Title: 배낭 채우기, Time: 126ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250202/배낭 채우기/knapsack.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Diamond {
+    int weight;
+    int value;
+
+    Diamond(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
+
+public class Main {
+    static int N, M;
+    static Diamond[] diamonds;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        diamonds = new Diamond[N];
+        dp = new int[M + 1];
+
+        for (int i = 0; i < N; i++) {
+            diamonds[i] = new Diamond(sc.nextInt(), sc.nextInt());
+
+
+            for (int j = M; j >= diamonds[i].weight + 1; j--) {
+                if (dp[j - diamonds[i].weight] != 0) {
+                    dp[j] = Math.max(dp[j - diamonds[i].weight] , dp[j - diamonds[i].weight] + diamonds[i].value);
+                }
+            }
+            dp[diamonds[i].weight] = Math.max(diamonds[i].value, dp[diamonds[i].weight]);
+
+//            System.out.println(Arrays.stream(dp).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+        }
+
+        int answer = 0;
+
+        for (int i : dp) {
+            answer = Math.max(answer, i);
+        }
+
+        System.out.println(answer);
+    }
+}
