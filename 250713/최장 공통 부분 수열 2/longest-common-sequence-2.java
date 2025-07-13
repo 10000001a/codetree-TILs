2027f8c7d75e3812f676a43e2d16db594e02415b
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-07-13 18:08:08 +0900

    Title: 최장 공통 부분 수열 2, Time: 113ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250713/최장 공통 부분 수열 2/longest-common-sequence-2.java
+++ 250713/최장 공통 부분 수열 2/longest-common-sequence-2.java
@@ -5,6 +5,7 @@ public class Main {
     static String a, b;
 
     static int[][] dp;
+    static int[][][] path;
 
     public static void main(String[] args) {
         final Scanner sc = new Scanner(System.in);
@@ -13,69 +14,105 @@ public class Main {
         b = sc.next();
 
         dp = new int[a.length() + 1][b.length() + 1];
+        path = new int[a.length() + 1][b.length() + 1][2];
 
         if (a.charAt(0) == b.charAt(0)) {
             dp[0][0] = 1;
+
         }
+
+        path[0][0][0] = -1;
+        path[0][0][1] = -1;
 
         for (int j = 1; j < b.length(); j++) {
             if (b.charAt(j) == a.charAt(0)) {
                 dp[0][j] = 1;
+                path[0][j][0] = -1;
+                path[0][j][1] = j - 1;
+
             } else {
                 dp[0][j] = dp[0][j - 1];
+                path[0][j][0] = 0;
+                path[0][j][1] = j - 1;
             }
+
+
         }
 
         for (int i = 1; i < a.length(); i++) {
             if (b.charAt(0) == a.charAt(i)) {
                 dp[i][0] = 1;
+                path[i][0][0] = i - 1;
+                path[i][0][1] = -1;
             } else {
                 dp[i][0] = dp[i - 1][0];
+                path[i][0][0] = i - 1;
+                path[i][0][1] = 0;
             }
+
+
         }
 
         for (int i = 1; i < a.length(); i++) {
             for (int j = 1; j < b.length(); j++) {
                 if (a.charAt(i) == b.charAt(j)) {
-                    if (dp[i - 1][j] == dp[i][j - 1]) {
-                        dp[i][j] = dp[i][j - 1] + 1;
-                    } else {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                    }
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    path[i][j][0] = i - 1;
+                    path[i][j][1] = j - 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
 
+                    if (dp[i - 1][j] <= dp[j][j - 1]) {
+                        path[i][j][0] = i;
+                        path[i][j][1] = j - 1;
+                    }
 
-        StringBuilder answer = new StringBuilder();
-
-        int answer_len = dp[a.length() - 1][b.length() - 1];
-
-        int start_x = a.length() - 1;
-        int start_y = b.length() - 1;
-
-        for (int z = 0; z < answer_len; z++) {
-            boolean break_flag = false;
-            for (int i = 0; i <= start_x; i++) {
-                for (int j = 0; j <= start_y; j++) {
-                    if (dp[i][j] == answer_len - z) {
-                        answer.insert(0, a.charAt(i));
-                        start_x = i - 1;
-                        start_y = j - 1;
-                        break_flag = true;
-                        break;
+                    if (dp[i - 1][j] > dp[j][j - 1]) {
+                        path[i][j][0] = i - 1;
+                        path[i][j][1] = j;
                     }
                 }
-
-                if (break_flag) {
-                    break;
-                }
             }
         }
-        System.out.println(answer);
+//
+//        StringBuilder x = new StringBuilder();
+//
+//        for (int i = 0; i < a.length(); i++) {
+//            for (int j = 0; j < b.length(); j++) {
+//                x.append("(").append(path[i][j][0]).append(", ").append(path[i][j][1]).append(") ");
+//            }
+//            x.append("\n");
+//        }
 
+//        System.out.println(x);
+
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (path[i][j][0] == i - 1 && path[i][j][1] == j - 1 && a.charAt(i) == b.charAt(j)) {
+
+                sb.append(a.charAt(i));
+                i--;
+                j--;
+            } else {
+                int a = path[i][j][0];
+                int b = path[i][j][1];
+
+                i = a;
+                j = b;
+            }
+
+            if (i == -1 && j == -1) {
+                break;
+            }
+//            System.out.println("i: " + i + ", j:" + j);
+        }
+
+        System.out.println(sb);
     }
 }
 // 0 0 0 0 1 1
@@ -87,3 +124,5 @@ public class Main {
 // 1 1 1 1
 // 1 2 2 2
 // 1 2 2 3
+//CXFVKGKFCBVXVGGCKKXCXBCVFBKGGFEKBCBFBCCXEBVVEVECGFCFBBBEGGXVVCVEFKGCFVKFCEFVKEEVGKXBGECCFXVVEGKKGEVFEXBGCXKEVXEXVEXKGGVEGCXEGGFGCXXFFXVKVEBVKVBXFXCFKEKEKFKCFBGBFFKBKGBBGCXFBCEVCGFGFGCEKGCGEEEBXXECVVKKBCCGCCFBGBBVFBXVCCBCCGEEXCXGGXBXBFGBGXKXGVVCEBFGVGKFKFCGBVGVEKCFXKEEXCFCGGCBECGKEFVGGGEG
+//CXFVKGKFCBVXVGGCKKXCXBCVFBKGGFEBCFBCCXEBVVEVECGFCFBBBEGGXVVCVEFKGCFVKFCEFVKEEVGKXBGECCFXVVEGKKGEVFEXBGCXKEVXEXVEXKGGVEGCXEGGFGCXXFFXVKVEBVKVBXFXCFKEKEKFKCFBGBFFKBKGBBGCXFBCEVCGFGFGCEKGCGEEBXXECVVKKBCCGCCFBGBBVFBXVCCBCCEEXCGGXBXBFGBGXKXGVVCEBFGVGKFKFCGBVGVEKCFXKEEXCFCGGCBECGKEFVGGGEG
