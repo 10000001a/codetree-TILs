6729d5201cab4973f56122f138b45100ed94c5cd
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-08-03 23:00:09 +0900

    Title: 패턴 매칭, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250803/패턴 매칭/pattern-matching.java
+++ 250803/패턴 매칭/pattern-matching.java
@@ -20,6 +20,10 @@ public class Main {
 
         for (int i = 1; i <= S.length(); i++) {
             for (int j = 1; j <= P.length(); j++) {
+                if (j > i) {
+                    dp[i][j] = dp[i][i];
+                    continue;
+                }
                 char pattern_char = P.charAt(j - 1);
 
                 switch (pattern_char) {
@@ -31,25 +35,44 @@ public class Main {
                         if (cmp == '.') {
                             dp[i][j] = dp[i - 1][j] + 1;
                         } else if (cmp == S.charAt(i - 1)) {
-                            dp[i][j] = dp[i - 1][j] + 1;
+                            if (dp[i - 1][j] < i - 1) {
+                                dp[i][j] = dp[i - 1][j];
+                            } else {
+                                dp[i][j] = dp[i - 1][j] + 1;
+                            }
+                            
                         } else {
-                            dp[i][j] = dp[i - 1][j];
+                            dp[i][j] = dp[i][j- 1];
                         }
                         break;
                     default:
                         if (pattern_char == S.charAt(i - 1)) {
-                            dp[i][j] = dp[i-1][j-1] + 1;
+                            if (dp[i][j - 1] < i - 1) {
+                                dp[i][j] = dp[i - 1][j];
+                            } else {
+                                dp[i][j] = dp[i - 1][j - 1] + 1;
+                            }
+                        } else {
+                            dp[i][j] = dp[i - 1][j];
                         }
                         break;
                 }
             }
         }
 
-        for (int i = 1; i <= P.length(); i++) {
-            if (dp[S.length()][i] == S.length()) {
-                System.out.println("true");
-                return;    
-            }
+        // StringBuilder sb = new StringBuilder();
+        // for (int i = 1; i <= S.length(); i++) {
+        //     for (int j = 1; j <= P.length(); j++) {
+        //         sb.append(dp[i][j]);
+        //         sb.append(" ");
+        //     }
+        //     sb.append("\n");
+        // }
+        // System.out.println(sb);
+
+        if (dp[S.length()][P.length()] == S.length()) {
+            System.out.println("true");
+            return;    
         }
         System.out.println("false");
 
