36eea1546faaa2f31e1ba660b19f9cd9913e8898
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2025-08-07 02:09:12 +0900

    Title: 1차원 윷놀이, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250807/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static int N, M, K;
+    static int[] nums;
+
+    static int[] positions;
+
+    static int answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+
+        nums = new int[N];
+        positions = new int[K];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for (int k = 0; k < K; k++) {
+            run(0, k);
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void run(int index, int k) {
+        positions[k] += nums[index];
+
+        if (index == N - 1) {
+            int tmp_answer = 0;
+
+            for (int pos: positions) {
+                if (pos >= M - 1) {
+                    tmp_answer++;
+                }
+            }
+
+            answer = Math.max(tmp_answer, answer);
+
+            return;
+        }
+
+        for (int newK = 0; newK < K; newK++) {
+            if (positions[newK] > M) {
+                continue;
+            }
+
+            run(index + 1, newK);
+            positions[newK] -= nums[index + 1];
+        }
+    }
+}
